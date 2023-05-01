7417a2144a2398daee8d2ded692bb181cb5bce4b
Rodolpho <redsmedeiros@gmail.com>
2023-05-01 16:35:47 -0300

    primeiro

---
+++ src/main/java/com/blackGram/BlackgramApplication.java
@@ -0,0 +1,13 @@
+package com.blackGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackGram/BlackgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackGram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
